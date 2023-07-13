9aa6b77a95290b24cb69e41f94bb3f346c4b235e
Minhazul-Arfin <arefinminhaz45@gmail.com>
2023-07-13 14:17:40 +0600

    data loaded successfully

---
+++ phoneHunter.js
@@ -0,0 +1,32 @@
+const loadData = () => {
+    const url = 'https://openapi.programming-hero.com/api/phones?search=iphone';
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhone(data.data))
+}
+
+const displayPhone = data => {
+    console.log(data);
+    const phoneContainer = document.getElementById('phone-container');
+
+    data.forEach(phone =>{
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+
+        phoneDiv.innerHTML = `
+        <div class="card p-5">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${phone.phone_name}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `;
+        phoneContainer.appendChild(phoneDiv);
+
+    });
+}
+
+
+loadData();
